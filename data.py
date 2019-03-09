55418a63a7791efd44aede6250c1fcef965acf55
zappyfish <liamkelly2015@gmail.com>
2019-03-09 12:26:55 -0500

    method 1

--- data.py
+++ data.py
@@ -14,3 +14,29 @@ class Study:
             'researcher': self.researcher,
             'description': self.full_description
         }
+
+
+class StudyNode:
+
+    def __init__(self, study):
+        self.study = study
+        self.dependencies = []  # What this study depends on
+        self.dependers = []  # What studies depend on this one
+
+    def add_dependency(self, node):
+        self.dependencies.append(node)
+
+    def add_depender(self, node):
+        self.dependers.append(node)
+
+class StudyGraph:
+
+    def __init__(self):
+        self.nodes = []
+
+    def add_study(self, study):
+        self.nodes.append(study)
+        self._rebuild_graph()
+
+    def _rebuild_graph(self):
+        pass
